6eb63b2666b9c2cd34052fd3dcef98aa12e2c00f
Celia050313 <celia.henry1@viacesi.fr>
2018-12-07 09:36:13 +0100

    Test

---
+++ Cuisine/UnitTestModel/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestModel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestModel")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78f745cb-ad25-4d59-908c-50f778b9ebb8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cuisine/UnitTestModel/TestADO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestModel
+{
+    [TestClass]
+    public class TestADO
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
