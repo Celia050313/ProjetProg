4cf29a93b4c3d7a11e102ec7022a8219924fd893
Arthur SIMON <arthur.simon@viacesi.fr>
2018-12-07 09:45:12 +0100

    Renommage conteneur batterie de test

--- Salle/TestProjetModel/Properties/AssemblyInfo.cs
+++ Salle/TestProjetModel/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ProjetTestModel")]
+[assembly: AssemblyTitle("TestProjetModel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProjetTestModel")]
+[assembly: AssemblyProduct("TestProjetModel")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("69ef0f5c-7e16-4f78-9065-1325186c99db")]
+[assembly: Guid("397ed25b-e506-4043-9f8c-14393d6254d7")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- Salle/TestProjetModel/UnitTest1.cs
+++ Salle/TestProjetModel/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace ProjetTestModel
+namespace TestProjetModel
 {
     [TestClass]
     public class UnitTest1
