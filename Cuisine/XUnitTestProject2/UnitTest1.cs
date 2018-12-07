d024563af9d88be0f854023d7cc86e27169126fb
Pierre THENOT <pierre.thenot@viacesi.fr>
2018-12-07 08:56:04 +0100

    wins and xunit

---
+++ Cuisine/XUnitTestProject2/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProjectCuisine
+{
+    public class TestFacadeControllerSimu
+
+
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
