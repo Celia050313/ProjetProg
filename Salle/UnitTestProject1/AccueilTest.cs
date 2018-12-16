877fec34f2eb03d5b60c0a7888d800522068b315
quentingnt <quentin.guenot@viacesi.fr>
2018-12-16 20:36:07 +0100

    ok

--- Salle/UnitTestProject1/AccueilTest.cs
+++ Salle/UnitTestProject1/AccueilTest.cs
@@ -1,57 +1,61 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestProject1
+
+namespace TestControllerSimu
 {
-    [TestClass]
-    public class Accueil
+    [TestClass()]
+    public class AccueilTest
     {
+        TestControllerSimu.Accueil acc = new TestControllerSimu.Accueil();
+
         [TestMethod()]
         public void accueil()
         {
-
+            Assert.Fail();
         }
 
         [TestMethod()]
-        public void paie()
+        public void paieTest()
         {
-
+            
+            Assert.AreEqual(23);
         }
 
         [TestMethod()]
         public void ordonner()
         {
-
+            Assert.Fail();
         }
 
         [TestMethod()]
         public void assigneTable()
         {
-
+           Assert.Fail();
         }
 
         [TestMethod()]
         public void groupe()
         {
-
+            Assert.Fail();
         }
 
         [TestMethod()]
         public void ordre()
         {
-
+            Assert.Fail();
         }
 
         [TestMethod()]
         public void wait()
         {
-
+            Assert.Fail();
         }
 
         [TestMethod()]
         public void reserver()
         {
-
+            Assert.Fail();
         }
     }
 }
